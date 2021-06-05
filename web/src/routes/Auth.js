082b7732f6c667c96cd8db729e5217d4319f988d
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-06-05 19:40:29 +0900

    Update Auth.js
    new account / log in 별로 처리 구현

--- web/src/routes/Auth.js
+++ web/src/routes/Auth.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import * as firebase from "common/firebase";
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
     const onChange = (event) => {
         const {
             target: { name, value }
@@ -16,18 +18,37 @@ const Auth = () => {
         }
     };
 
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
         // 기본 새로고침 x
         event.preventDefault();
+        try {
+            let data;
+
+            if (newAccount) {
+                // create account
+                data = await firebase.auth.createUserWithEmailAndPassword(email, password);
+            } else {
+                // log in
+                data = await firebase.auth.signInWithEmailAndPassword(email, password);
+            }
+
+            console.log(data);
+        } catch (err) {
+            setError(err.message);
+        }
     };
+
+    const toggleAccount = () => setNewAccount((prev) => !prev);
 
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange} />
                 <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
-                <input type="submit" value="Log In" />
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+                {error ? `Error : ${error}` : ""}
             </form>
+            <span onClick={toggleAccount}>{newAccount ? "Sign in" : "Create Account"}</span>
             <div>
                 <button>Continue with Google</button>
                 <button>Continue with Github</button>
