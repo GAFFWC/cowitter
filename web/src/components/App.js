85761becad8b1168e7a4190757de45ffe4f3a798
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-06-05 18:54:29 +0900

    Update App.js
    auth 조건으로 라우팅

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import AppRouter from "./Router";
+import * as firebase from "common/firebase";
 
 function App() {
-    return <AppRouter />;
+    console.log(firebase.auth.currentUser);
+
+    const [isLoggedIn, setIsLoggedIn] = useState(firebase.auth.currentUser);
+    return (
+        <>
+            <AppRouter isLoggedIn={isLoggedIn} />;
+            <footer>&copy; Cowitter {new Date().getFullYear()}</footer>
+        </>
+    );
 }
 
 export default App;
