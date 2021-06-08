8415c3c359767f448feff3a740bce10ba6acf093
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-06-08 21:09:26 +0900

    Update firebase.js

--- web/src/common/firebase.js
+++ web/src/common/firebase.js
@@ -1,17 +1,16 @@
-import * as fb from "firebase";
+import firebase from "firebase";
 import "firebase/auth";
+import "firebase/firestore";
 
 const config = {
     apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_MAIN,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGEING_SENDER_ID,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID
 };
 
-const firebase = fb.default.initializeApp(config);
+firebase.initializeApp(config);
 
-console.log(firebase);
-
-export const auth = firebase.auth();
+export const fb = firebase;
