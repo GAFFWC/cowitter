e39ef2c62e0be50ff9b28b60e2c5f0816a702dfa
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-06-08 21:10:10 +0900

    Update index.js

--- web/src/index.js
+++ web/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import * as firebase from "common/firebase";
 
 ReactDOM.render(
     <React.StrictMode>
