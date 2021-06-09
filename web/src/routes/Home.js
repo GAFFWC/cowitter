f5fb2fdec6e0cf9830523573e42cd00b0e4e0de9
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-06-09 21:54:49 +0900

    Update Home.js
    파일 업로드 & 삭제 기능 추가

--- web/src/routes/Home.js
+++ web/src/routes/Home.js
@@ -2,10 +2,11 @@ import { fb } from "common/firebase";
 import Cowit from "components/Cowit";
 import moment from "moment";
 import React, { useEffect, useState } from "react";
-
+import { v4 as uuidv4 } from "uuid";
 const Home = ({ userData }) => {
     const [cowit, setCowit] = useState("");
     const [cowits, setCowits] = useState([]);
+    const [attachment, setAttachment] = useState();
 
     const getCowits = async () => {
         (await fb.firestore().collection("cowits").get()).forEach((doc) => {
@@ -31,15 +32,26 @@ const Home = ({ userData }) => {
 
     const onCowitSubmit = async (event) => {
         event.preventDefault();
-        await fb
-            .firestore()
-            .collection("cowits")
-            .add({
-                userUid: userData.uid,
-                text: cowit,
-                createdAt: moment(new Date(), "YYYY-MM-DD HH:mm:ss").toString()
+
+        const newCowit = {
+            text: cowit,
+            createdAt: moment(new Date(), "YYYY-MM-DD HH:mm:ss").toString(),
+            userUid: userData.uid
+        };
+
+        if (attachment) {
+            const attachmentRef = fb.storage().ref().child(`${userData.uid}/${uuidv4()}`);
+
+            const attachmentUrl = await attachmentRef.putString(attachment, "data_url").then(async (r) => {
+                return await r.ref.getDownloadURL();
             });
+
+            newCowit.attachment = attachmentUrl;
+        }
+
+        await fb.firestore().collection("cowits").add(newCowit);
         setCowit("");
+        setAttachment(null);
     };
 
     const onChange = (event) => {
@@ -50,11 +62,39 @@ const Home = ({ userData }) => {
         setCowit(value);
     };
 
+    const onFileChange = (event) => {
+        const {
+            target: { files }
+        } = event;
+
+        const file = files[0];
+        const reader = new FileReader();
+
+        reader.readAsDataURL(file);
+
+        reader.onloadend = (finished) => {
+            const {
+                currentTarget: { result }
+            } = finished;
+            setAttachment(result);
+        };
+    };
+
+    const onClearAttachmentClick = () => {
+        setAttachment(null);
+    };
     return (
         <div>
             <form onSubmit={onCowitSubmit}>
                 <input value={cowit} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+                <input type="file" accept="image/*" onChange={onFileChange} />
                 <input type="submit" value="Cowit" onSubmit={onCowitSubmit} />
+                {attachment && (
+                    <div>
+                        <img src={attachment} width="50px" height="50px" />
+                        <button onClick={onClearAttachmentClick}>Clear</button>
+                    </div>
+                )}
             </form>
             <div>
                 {cowits.map((cowit) => (
