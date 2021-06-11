cf500fd9da45266da4ab43578de8f772f2a78120
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-06-11 09:43:26 +0900

    Update Navigation.js
    userData 공유

--- web/src/components/Navigation.js
+++ web/src/components/Navigation.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Navigation = () => (
+const Navigation = ({ userData }) => (
     <nav>
         <ul>
             <li>
                 <Link to="/">Home</Link>
             </li>
             <li>
-                <Link to="/profile">My Profile</Link>
+                <Link to="/profile">{userData.displayName}의 My Profile</Link>
             </li>
         </ul>
     </nav>
