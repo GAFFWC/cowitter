5c562893e9a92fd7b6e805e4c036800269f5af57
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-06-13 21:17:22 +0900

    Create AuthForm.js

---
+++ web/src/components/AuthForm.js
@@ -0,0 +1,58 @@
+import { fb } from "common/firebase";
+import React, { useState } from "react";
+
+const AuthForm = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
+    const onChange = (event) => {
+        const {
+            target: { name, value }
+        } = event;
+
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    };
+
+    const onSubmit = async (event) => {
+        // 기본 새로고침 x
+        event.preventDefault();
+        try {
+            let data;
+
+            if (newAccount) {
+                // create account
+                data = await fb.auth().createUserWithEmailAndPassword(email, password);
+            } else {
+                // log in
+                data = await fb.auth().signInWithEmailAndPassword(email, password);
+            }
+
+            console.log(data);
+        } catch (err) {
+            setError(err.message);
+        }
+    };
+
+    const toggleAccount = () => setNewAccount((prev) => !prev);
+
+    return (
+        <>
+            <form onSubmit={onSubmit} className="container">
+                <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange} className="authInput" />
+                <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} className="authInput" />
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"} className="authInput authSubmit" />
+                {error && <span className="authError">{error}</span>}
+            </form>
+            <span onClick={toggleAccount} className="authSwitch">
+                {newAccount ? "Sign in" : "Create Account"}
+            </span>
+        </>
+    );
+};
+
+export default AuthForm;
