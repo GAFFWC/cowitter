c7498ec7fc1af2aceb0ddda3b8a2060b3d49d108
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-06-16 23:16:51 +0900

    Update app.module.ts

--- auth/src/app.module.ts
+++ auth/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { KakaoModule } from "./kakao/kakao.module";
 import { GoogleModule } from "./google/google.module";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-    imports: [KakaoModule, GoogleModule],
+    imports: [MongooseModule.forRoot("mongodb://localhost:27017"), KakaoModule, GoogleModule],
     controllers: [],
     providers: []
 })
