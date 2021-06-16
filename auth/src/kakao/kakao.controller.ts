ea117ed465938d6dd31d1940f0a6083ee9235408
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-06-16 23:17:07 +0900

    Update kakao.controller.ts

--- auth/src/kakao/kakao.controller.ts
+++ auth/src/kakao/kakao.controller.ts
@@ -1,40 +1,38 @@
 import { JsonHeaders } from "@cowitter/decorators";
-import { Controller, Get, Headers, HttpService, InternalServerErrorException, Post, Req } from "@nestjs/common";
+import { Body, Controller, Get, Headers, HttpService, InternalServerErrorException, Post, Req } from "@nestjs/common";
 import { KakaoAuth } from "./dto/kakao.dto";
 
 @Controller("kakao")
 export class KakaoController {
-    constructor(private readonly http: HttpService) {}
+    constructor(private readonly http: HttpService, private readonly kakaoService) {}
     @Get()
     async get(@Req() req) {
         console.log(req);
     }
 
     @Post("login")
-    async post(@JsonHeaders("auth") kakaoAuth: KakaoAuth) {
-        try {
-            console.log(kakaoAuth);
+    async post(@Body() kakaoAuth: KakaoAuth) {
+        console.log(kakaoAuth);
 
-            const userData = this.http
-                .get("https://kapi.kakao.com/v2/user/me", {
-                    headers: {
-                        Authorization: `Bearer ${kakaoAuth.access_token}`
-                    }
-                })
-                .toPromise()
-                .then((r) => {
-                    return r.data;
-                })
-                .catch((err) => {
-                    console.error(err);
-                    return null;
-                });
+        const userData = await this.http
+            .get("https://kapi.kakao.com/v2/user/me", {
+                headers: {
+                    Authorization: `Bearer ${kakaoAuth.access_token}`
+                }
+            })
+            .toPromise()
+            .then((r) => r.data)
+            .catch((err) => {
+                console.error(err);
+                return null;
+            });
 
-            if (!userData) {
-                throw new InternalServerErrorException("Get user info failed");
-            }
-        } catch (err) {
-            console.error(err);
+        if (!userData) {
+            throw new InternalServerErrorException("Get user info failed");
         }
+
+        console.log(userData);
+
+        const user = 
     }
 }
