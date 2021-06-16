8f194da81f85639e3a7fb0500a3ad4cc4b0f6011
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-06-16 23:17:35 +0900

    Update App.js

--- web-v2/src/App.js
+++ web-v2/src/App.js
@@ -2,6 +2,7 @@ import LoginWithGoogle from "./components/Google";
 import LoginWithKakao from "./components/Kakao";
 
 function App() {
+    console.log(process.env);
     return (
         <div className="App">
             <LoginWithKakao />
