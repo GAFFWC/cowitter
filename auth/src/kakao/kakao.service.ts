0b8ee5e1355d0b140925f46775c7b0d741c7d536
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-06-18 09:17:44 +0900

    Update kakao.service.ts

--- auth/src/kakao/kakao.service.ts
+++ auth/src/kakao/kakao.service.ts
@@ -1,14 +1,4 @@
-import { CreateUserVO, User } from "@cowitter/schema";
 import { Injectable } from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
-import { Document, Model } from "mongoose";
 
 @Injectable()
-export class KakaoService {
-    constructor(@InjectModel(User.name) private readonly user: Model<User>) {}
-
-    createUser = async (createUserVO: CreateUserVO): Promise<User & Document<any, any>> => {
-        const newUser = new this.user(createUserVO);
-        return await newUser.save();
-    };
-}
+export class KakaoService {}
