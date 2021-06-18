90df6d32b0666e611cd9be788ac9a3c09eac872b
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-06-18 09:17:40 +0900

    Add UserModule

--- auth/src/google/google.module.ts
+++ auth/src/google/google.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
+import { UserModule } from "src/user/user.module";
 import { GoogleController } from "./google.controller";
 
 @Module({
+    imports: [UserModule],
     controllers: [GoogleController]
 })
 export class GoogleModule {}
--- auth/src/kakao/kakao.module.ts
+++ auth/src/kakao/kakao.module.ts
@@ -1,8 +1,9 @@
 import { HttpModule, Module } from "@nestjs/common";
+import { UserModule } from "src/user/user.module";
 import { KakaoController } from "./kakao.controller";
 
 @Module({
-    imports: [HttpModule],
+    imports: [HttpModule, UserModule],
     controllers: [KakaoController]
 })
 export class KakaoModule {}
