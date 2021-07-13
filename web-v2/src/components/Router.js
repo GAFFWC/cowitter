f812afaed29e1f7eb50a009ab96b3aa7d6035913
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-07-13 16:32:59 +0900

    Update Router.js

--- web-v2/src/components/Router.js
+++ web-v2/src/components/Router.js
@@ -1,5 +1,34 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "./Auth";
+import Home from "./Home";
 const AppRouter = () => {
-    return <Router></Router>;
+    const { login, userData } = useSelector((state) => ({
+        login: state.user.login,
+        userData: state.user.userData
+    }));
+
+    console.log(login);
+    console.log(userData);
+
+    return (
+        <Router>
+            {/* for Navigation */}
+            <Switch>
+                {login ? (
+                    <Home />
+                ) : (
+                    // for home, ...
+                    <>
+                        <Route>
+                            <Auth login={login} userData={userData} />
+                        </Route>
+                    </>
+                )}
+            </Switch>
+        </Router>
+    );
 };
+
+export default AppRouter;
