2249ecc78bfbd0794e9e3d64695fff58cbedd8f9
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-07-13 16:32:22 +0900

    redux 세팅

--- web-v2/src/index.js
+++ web-v2/src/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { CookiesProvider } from "react-cookie";
+import { createStore } from "redux";
+import rootReducer from "./modules";
+import { Provider } from "react-redux";
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <CookiesProvider>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </CookiesProvider>
     </React.StrictMode>,
     document.getElementById("root")
 );
